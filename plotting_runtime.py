7136a2304f68888497b48f0cf3cdfca4198999bb
Ida Dittrich <idittrich@student.ethz.ch>
2024-07-14 12:12:58 +0200

    made multiple runtime files

--- plotting_runtime.py
+++ plotting_runtime.py
@@ -1,8 +1,11 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 dimensions = []
 runtimes = []
+likelihood = []
+likelihood_err = []
 
 with open('runtime.csv', 'r') as file:
     reader = csv.reader(file)
@@ -10,13 +13,30 @@ with open('runtime.csv', 'r') as file:
     for row in reader:
         dimensions.append(int(row[0]))
         runtimes.append(float(row[1]))
+        likelihood.append(float(row[2]))
+        likelihood_err.append(float(row[4]))
 
-plt.figure(figsize=(10, 6))
-plt.plot(dimensions, runtimes, marker='o', linestyle='-', color='b')
-plt.xlabel('Dimensions')
-plt.ylabel('Elapsed Time (seconds)')
+
+
+fig, ax1 = plt.subplots()
+
+ax1.plot(dimensions, runtimes, marker='o', linestyle='-', color='b')
+ax1.set_xlabel('Dimensions')
+ax1.set_ylabel('Elapsed Time (seconds)', color='b')
+ax1.set_yscale('log')
+ax1.set_xscale('log')
+ax1.tick_params(axis='y', labelcolor='b')
+
+ax1.set_xlim([1, 10])
+
+ax2 = ax1.twinx()
+ax2.errorbar(dimensions, np.abs(likelihood), yerr=likelihood_err, color='r')
+ax2.set_ylabel('Loglikelihood', color='r')
+#ax2.set_xscale('log')
+ax2.tick_params(axis='y', labelcolor='r')
+
+ax1.grid(True)
+
 plt.title('Runtime of Nested Sampling on the Rosenbrock Function')
-plt.grid(True)
-plt.yscale('log')  # Use logarithmic scale for better visualization
-plt.xscale('log')  # Use logarithmic scale for better visualization
+
 plt.show()
